3820442512ef8fc035402d8f311fcbb375cd662a
HOANGMAI\DELL <haphuonghoangmai@gmail.com>
2024-12-17 01:44:42 +0700

    update

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -18,26 +18,6 @@ const Character: React.FC<CharacterProps> = ({
   size = CHARACTER_RIGHT.size,
 }) => {
   const characterRef = useRef<HTMLDivElement>(null);
-  const handleMove = (e: KeyboardEvent) => {
-    let translateXValue = 0;
-    let translateYValue = 0;
-    const regex = /translateX\(([^)]+)\)\s*translateY\(([^)]+)\)/;
-    const transformValue = characterRef.current?.style.transform;
-    if (transformValue) {
-      const match = transformValue.match(regex);
-      if (match) {
-        translateXValue = parseFloat(match[1]);
-        translateYValue = parseFloat(match[2]);
-      }
-    }
-    if (e.key === "ArrowRight") {
-      const x = translateXValue + 3;
-      const y = translateYValue;
-      if (characterRef.current) {
-        characterRef.current.style.transform = `translateX(${x}rem) translateY(${y}rem)`;
-      }
-    }
-  };
   const getTransformPosition = () => {
     const x = 3 * position.col;
     const y = 3 * position.row;
